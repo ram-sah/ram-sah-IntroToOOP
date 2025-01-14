d403dc281da23c8ca2bbd9ab337517025f5c08c7
Ram Sah <mohan2036@gmail.com>
2025-01-14 23:35:06 +0000

    custom datatype, instantiation

--- src/main/java/Bird.java
+++ src/main/java/Bird.java
@@ -45,7 +45,8 @@ public class Bird {
      *         greater than or equal to 3.
      */
     public String sound(){
-        return "";
+        return (ageMonths < 3 ) ? "Tweet" : "Chirp" ;
+        
     }
 
 }
